fadd70247fdda5a62253b37ea05a98568ddcd6e2
dongitonga <donivardgitonga@gmail.com>
2023-01-10 02:53:26 +0300

    Debug and fixes

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ class Calculator {
     this.currentOperandTextElement = currentOperandTextElement;
     this.clear();
   }
+
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
-    this.operation = undifined;
+    this.operation = undefined;
   }
+
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
+
   appendNumber(number) {
     if (number === "." && this.currentOperand.includes(".")) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
+
   chooseOperation(operation) {
     if (this.currentOperand === "") return;
     if (this.previousOperand !== "") {
@@ -25,6 +29,7 @@ class Calculator {
     this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
+
   compute() {
     let computation;
     const prev = parseFloat(this.previousOperand);
@@ -38,7 +43,7 @@ class Calculator {
         computation = prev - current;
         break;
       case "*":
-        computation = prev - current;
+        computation = prev * current;
         break;
       case "÷":
         computation = prev / current;
@@ -50,22 +55,23 @@ class Calculator {
     this.operation = undefined;
     this.previousOperand = "";
   }
+
   getDisplayNumber(number) {
     const stringNumber = number.toString();
-    const intergerDigits = parseFloat(stringNumber.split(".")[0]);
-    const decimalDigits = parseFloat(stringNumber.split(".")[1]);
-    let intergerDisplay;
-    if (isNaN(intergerDigits)) {
-      intergerDisplay = "";
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
     } else {
-      intergerDisplay = intergerDigits.toLocaleString("en", {
+      integerDisplay = integerDigits.toLocaleString("en", {
         maximumFractionDigits: 0,
       });
     }
     if (decimalDigits != null) {
-      return `${intergerDisplay}.${decimalDigits}`;
+      return `${integerDisplay}.${decimalDigits}`;
     } else {
-      return intergerDisplay;
+      return integerDisplay;
     }
   }
 
@@ -106,20 +112,24 @@ numberButtons.forEach((button) => {
     calculator.updateDisplay();
   });
 });
+
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
 });
+
 equalsButton.addEventListener("click", (button) => {
   calculator.compute();
   calculator.updateDisplay();
 });
+
 allClearButton.addEventListener("click", (button) => {
   calculator.clear();
   calculator.updateDisplay();
 });
+
 deleteButton.addEventListener("click", (button) => {
   calculator.delete();
   calculator.updateDisplay();
